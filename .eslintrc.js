8fb16f72092dc23ac71bba99b77d4fd46a33ee56
Chaehoon Lim <sizplay@gmail.com>
2021-05-10 23:13:10 +0900

    스타일 설정 완료

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module",
+        "ecmaVersion": "2018",
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "quotes": ["error", "double"],
+        "@typescript-eslint/quotes": ["error", "double"],
+        "no-unused-vars": "off",
+        "spaced-comment":"off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "jsx-a11y/control-has-associated-label": "off",
+        "react/no-array-index-key": "off",
+        "comma-dangle": "off",
+        "arrow-body-style": "off",
+        "react/no-unescaped-entities": "off",
+        "react/prop-types": "off",
+        "object-curly-newline": "off",
+        "react/jsx-one-expression-per-line": "off",
+        "implicit-arrow-linebreak": "off",
+        "no-shadow": "off",
+        "operator-linebreak": "off",
+        "react/react-in-jsx-scope": "off",
+        "reawct/jsx-props-no-spreading": "off",
+        "jsx-a11y/anchor-is-vaild": "off",
+        "global-require": "off",
+        "react/jsx-filename-extension": [
+            1,
+            { "extensoins": [".js", ".jsx", ".tsx"]},
+        ],
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+                "js": "never",
+                "jsx": "never",
+                "ts": "never",
+                "tsx": "never"
+            } // import시 확장자명은 사용핟지 않는다
+        ]
+    },
+    "settings": {
+        "import/resolver": {
+            "node": {
+                "extensoins": [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
+            }
+        }
+    }
+};
